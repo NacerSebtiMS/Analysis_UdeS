cd6f51e7bb5548cec9b27e2c508b22f23adebc1e
NacerSebtiMS <nacersebti.ms@gmail.com>
2018-09-23 15:15:02 -0400

    K_PPV done

--- K_PPV.m
+++ K_PPV.m
@@ -15,9 +15,23 @@ function classed = K_PPV(rp,rnp,c,c1,c2)
 % end
 classed = cat(2,c,zeros(length(c),1));
 for i = 1:length(c)
-   min = [dist(rn(i,:)) c1];
-   for a = 1:length(rp)
-       
+   min = [dist(rp(1,:),c(i,:)) c1];
+   for a = 2:length(rp)
+       d = dist(rp(a,:),c(i,:));
+       if d < min(1)
+           min = [d c1];
+       end
    end
-classed = r;
+   for a = 1:length(rnp)
+       d = dist(rnp(a,:),c(i,:));
+       if d < min(1)
+           min = [d c2];
+       end
+   end
+   classed(i, length(classed(1,:))) = min(2);
+%    if min(2) == c1
+%        rp = cat(1,rp,c(i,:));
+%    else
+%        rnp = cat(1,rnp,c(i,:));
+%    end
 end
