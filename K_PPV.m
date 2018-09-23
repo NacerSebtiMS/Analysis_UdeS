33c8d55a62e3fb8ff147e9a9e0988c27d691cd8c
NacerSebtiMS <nacersebti.ms@gmail.com>
2018-09-23 14:19:02 -0400

    Added gitignore

--- K_PPV.m
+++ K_PPV.m
@@ -1,21 +1,23 @@
 function classed = K_PPV(rp,rnp,c,c1,c2)
-r = zeros( length(rp) + length(rnp) , length(rp(1,:))+1 );
-
-for i = 1:length(rp)
-    for j = 1:length(rp(1,:))
-        r(i,j) = rp(i,j);
-    end
-    r(i,length(r(1,:))) = c1;
-end
-for i = length(rp)+1:length(r)
-    for j = 1:length(rnp(1,:))
-        r(i,j) = rnp(i-length(rp),j);
-    end
-    r(i,length(r(1,:))) = c2;
-end
-
+% r = zeros( length(rp) + length(rnp) , length(rp(1,:))+1 );
+% 
+% for i = 1:length(rp)
+%     for j = 1:length(rp(1,:))
+%         r(i,j) = rp(i,j);
+%     end
+%     r(i,length(r(1,:))) = c1;
+% end
+% for i = length(rp)+1:length(r)
+%     for j = 1:length(rnp(1,:))
+%         r(i,j) = rnp(i-length(rp),j);
+%     end
+%     r(i,length(r(1,:))) = c2;
+% end
+classed = cat(2,c,zeros(length(c),1));
 for i = 1:length(c)
-   min = [  
-end
+   min = [dist(rn(i,:)) c1];
+   for a = 1:length(rp)
+       
+   end
 classed = r;
 end
