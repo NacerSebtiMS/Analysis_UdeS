738618d168dc8db640410553b5b193bce9f3ab36
NacerSebtiMS <nacersebti.ms@gmail.com>
2018-09-27 14:05:17 -0400

    Added K-PPV using barycentres

--- mainAPP2.m
+++ mainAPP2.m
@@ -84,9 +84,10 @@ covarianceP300_both_ref = (m*m')/(length(P300_both_ref)-1);
 [vecp_NP300,valp]= eig(covarianceP300N_ref)
 
 % Rot = [vecp_P300(:,3) vecp_P300(:,4) vecp_NP300(:,3) vecp_NP300(:,4)]
-Rot = [vecp_P300(:,3) vecp_NP300(:,4)]
+Rot = [vecp_P300(:,1) vecp_NP300(:,4)]
 % Rot = [vecp_P300(:,1) vecp_P300(:,4)]
 % Rot = vecp_P300 * vecp_NP300
+% Rot = eye(4);
 R1 = ref_P300 * Rot;
 
 R2 = ref_NP300 * Rot;
@@ -164,10 +165,20 @@ ref_both_dec = cat(1,[ref_P300_dec(:,1) ref_P300_dec(:,4)],[ref_NP300_dec(:,1) r
 
 %% k-PPV
 L = length(Rot(1,:))+1;
-R = K_PPV(1,R1,R2,test_P300*Rot,300,315);
-gud_P300 = (sum(R(:,L)==300)/length(R(:,L)))*100
-kppvNP300 = K_PPV(1,R1,R2,test_NP300*Rot,300,315);
-gud_NP300 = (sum(kppvNP300(:,L)==315)/length(kppvNP300(:,L)))*100
+
+R = K_PPV(135,R1,R2,test_P300*Rot,300,315);
+gud_P300 = (sum(R(:,L)==300)/length(R(:,L)))*100;
+fprintf('%2.2f%% de reussite P300\n',gud_P300);
+kppvNP300 = K_PPV(135,R1,R2,test_NP300*Rot,300,315);
+gud_NP300 = (sum(kppvNP300(:,L)==315)/length(kppvNP300(:,L)))*100;
+fprintf('%2.2f%% de reussite NP300\n',gud_NP300);
+
+kppvP300 = K_baryPPV(1,R1,R2,test_P300*Rot,300,315);
+gud_P300 = (sum(kppvP300(:,L)==300)/length(kppvP300(:,L)))*100;
+fprintf('%2.2f%% de reussite P300\n',gud_P300);
+kppvNP300 = K_baryPPV(1,R1,R2,test_NP300*Rot,300,315);
+gud_NP300 = (sum(kppvNP300(:,L)==315)/length(kppvNP300(:,L)))*100;
+fprintf('%2.2f%% de reussite NP300\n',gud_NP300);
 % C_kppv = K_PPV(1,[ref_P300_dec(:,1) ref_P300_dec(:,4)],[ref_NP300_dec(:,1) ref_NP300_dec(:,4)],test_both_p300_dec,0,1);
 % [C1,C2] = PlotPPV(test_both_p300_dec,C_kppv(:,3));
 
@@ -283,7 +294,7 @@ cov_ref_vectors_images = (m*m')/(Li-1);
 x1 = randn(length(ref_foret),1);
 x2 = randn(length(ref_coast),1);
 x3 = randn(length(ref_street),1);
-   
+if 0  
 figure
 subplot(2,3,1)
 plot(x1,ref_foret(:,1),'o',x2,ref_coast(:,1),'*',x3,ref_street(:,1),'+');
@@ -391,7 +402,7 @@ title('Lab: c3')
 
 
 disp(['street_count:',num2str(street_count),'  coast_count:',num2str(coast_count),'  foret_count:',num2str(foret_count)]);
-
+end
 %% Classification à l'aide d'une méthode paramétrique (supervisé ou non)
 
 %% Testes avec deux experiences différentes (présentations différentes de couleurs des images)
